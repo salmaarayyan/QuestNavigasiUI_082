57bb09d1bd2cf941a424e57c075635e312ea02b8
salmaarayyan <salmaarayyan@gmail.com>
2025-11-06 13:50:45 +0700

    Menambahkan composable TampilData dengan data contoh

--- app/src/main/java/com/example/navigasiku/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -1,6 +1,19 @@
 package com.example.navigasiku.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.navigasiku.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TampilData(){}
+fun TampilData(
+    onBackBtnClick:()->Unit
+){
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
+        Pair(stringResource(id = R.string.alamat), "Yogyakarta")
+    )
+
+}
