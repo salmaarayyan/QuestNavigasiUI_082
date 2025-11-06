29e89d3676158f4b3a665f8d5bb5e61b30660639
salmaarayyan <salmaarayyan@gmail.com>
2025-11-06 13:12:36 +0700

    Menambahkan anotasi OptIn untuk ExperimentalMaterial3Api

--- app/src/main/java/com/example/navigasiku/view/Formulir.kt
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -1,6 +1,11 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
 package com.example.navigasiku.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun FormIsian(
