f96d026ee9dae592f8169232bf73c62c98c2b94f
salmaarayyan <salmaarayyan@gmail.com>
2025-11-06 13:29:54 +0700

    Menambahkan form input Nama Lengkap di dalam Column

--- app/src/main/java/com/example/navigasiku/view/Formulir.kt
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -1,16 +1,24 @@
 @file:OptIn(ExperimentalMaterial3Api::class)
 package com.example.navigasiku.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.navigasiku.R
 
 @Composable
 fun FormIsian(
@@ -20,13 +28,27 @@ fun FormIsian(
     Scaffold(modifier = Modifier,
         {
             TopAppBar(
-                title = {Text(stringResource(id=R.string.home),
+                title = {Text(stringResource(id = R.string.home),
                     color = Color.White)},
                 colors = TopAppBarDefaults.topAppBarColors(
                     colorResource(id = R.color.teal_700)
                 )
             )
         }){ isiRuang ->
+        Column(modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally)
+        {
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(250.dp),
+                label = {Text(text = "Nama Lengkap")},
+                onValueChange = {},
+            )
+        }
 
     }
 }
