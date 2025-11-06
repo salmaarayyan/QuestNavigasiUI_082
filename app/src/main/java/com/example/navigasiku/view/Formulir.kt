9eaf19d4530c0e60d4c95e262b8a859e624ce988
salmaarayyan <salmaarayyan@gmail.com>
2025-11-06 13:37:14 +0700

    Menambahkan pilihan RadioButton untuk jenis kelamin di Row

--- app/src/main/java/com/example/navigasiku/view/Formulir.kt
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -3,12 +3,14 @@ package com.example.navigasiku.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -54,6 +56,18 @@ fun FormIsian(
                 .padding(20.dp)
                 .width(250.dp), thickness = Thickness,color = Color.Red
             )
+            Row{
+                jenisK.forEach {
+                    item->
+                    Row(verticalAlignment = Alignment.CenterHorizontally){
+                        RadioButton(
+                            selected = false,
+                            onClick = {item}
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
 
         }
 
