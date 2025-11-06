6f40a63f23165fddee07cb9d0e6718cdddb66a02
salmaarayyan <salmaarayyan@gmail.com>
2025-11-06 13:18:49 +0700

    Menambahkan Scaffold dengan TopAppBar pada composable

--- app/src/main/java/com/example/navigasiku/view/Formulir.kt
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -3,14 +3,30 @@ package com.example.navigasiku.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 
 @Composable
 fun FormIsian(
     jenisK:List<String> = listOf("Laki-laki", "Perempuan"),
     OnSubmitBtnClick : () -> Unit
 ){
+    Scaffold(modifier = Modifier,
+        {
+            TopAppBar(
+                title = {Text(stringResource(id=R.string.home),
+                    color = Color.White)},
+                colors = TopAppBarDefaults.topAppBarColors(
+                    colorResource(id = R.color.teal_700)
+                )
+            )
+        }){ isiRuang ->
 
+    }
 }
